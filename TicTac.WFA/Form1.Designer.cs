cff884a838ce7bb0d5e3ffc6bcee7a69f388d10d
Sergey Pochebyt <sergeymailmobile@gmail.com>
2019-02-12 13:13:10 +0300

    Tic.Tac

---
+++ TicTac.WFA/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace TicTac.WFA
+{
+    partial class TicTac
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A00 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.NGButton = new System.Windows.Forms.Button();
+            this.RButton = new System.Windows.Forms.Button();
+            this.EBotton = new System.Windows.Forms.Button();
+            this.XWin = new System.Windows.Forms.Label();
+            this.OWin = new System.Windows.Forms.Label();
+            this.Draws = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // A00
+            // 
+            this.A00.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A00.Location = new System.Drawing.Point(12, 12);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(85, 85);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = true;
+            this.A00.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A10
+            // 
+            this.A10.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A10.Location = new System.Drawing.Point(12, 103);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(85, 85);
+            this.A10.TabIndex = 1;
+            this.A10.UseVisualStyleBackColor = true;
+            this.A10.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A20
+            // 
+            this.A20.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A20.Location = new System.Drawing.Point(12, 194);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(85, 85);
+            this.A20.TabIndex = 2;
+            this.A20.UseVisualStyleBackColor = true;
+            this.A20.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A21
+            // 
+            this.A21.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A21.Location = new System.Drawing.Point(103, 194);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(85, 85);
+            this.A21.TabIndex = 5;
+            this.A21.UseVisualStyleBackColor = true;
+            this.A21.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A11
+            // 
+            this.A11.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A11.Location = new System.Drawing.Point(103, 103);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(85, 85);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A01
+            // 
+            this.A01.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A01.Location = new System.Drawing.Point(103, 12);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(85, 85);
+            this.A01.TabIndex = 3;
+            this.A01.UseVisualStyleBackColor = true;
+            this.A01.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A22
+            // 
+            this.A22.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A22.Location = new System.Drawing.Point(194, 194);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(85, 85);
+            this.A22.TabIndex = 8;
+            this.A22.UseVisualStyleBackColor = true;
+            this.A22.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A12
+            // 
+            this.A12.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A12.Location = new System.Drawing.Point(194, 103);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(85, 85);
+            this.A12.TabIndex = 7;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A02
+            // 
+            this.A02.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A02.Location = new System.Drawing.Point(194, 12);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(85, 85);
+            this.A02.TabIndex = 6;
+            this.A02.UseVisualStyleBackColor = true;
+            this.A02.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // NGButton
+            // 
+            this.NGButton.Location = new System.Drawing.Point(12, 298);
+            this.NGButton.Name = "NGButton";
+            this.NGButton.Size = new System.Drawing.Size(85, 25);
+            this.NGButton.TabIndex = 9;
+            this.NGButton.Text = "New Game";
+            this.NGButton.UseVisualStyleBackColor = true;
+            this.NGButton.Click += new System.EventHandler(this.NGButton_Click);
+            // 
+            // RButton
+            // 
+            this.RButton.Location = new System.Drawing.Point(103, 298);
+            this.RButton.Name = "RButton";
+            this.RButton.Size = new System.Drawing.Size(85, 25);
+            this.RButton.TabIndex = 10;
+            this.RButton.Text = "Reset";
+            this.RButton.UseVisualStyleBackColor = true;
+            this.RButton.Click += new System.EventHandler(this.RButton_Click);
+            // 
+            // EBotton
+            // 
+            this.EBotton.Location = new System.Drawing.Point(194, 298);
+            this.EBotton.Name = "EBotton";
+            this.EBotton.Size = new System.Drawing.Size(85, 25);
+            this.EBotton.TabIndex = 11;
+            this.EBotton.Text = "Exit";
+            this.EBotton.UseVisualStyleBackColor = true;
+            this.EBotton.Click += new System.EventHandler(this.EBotton_Click);
+            // 
+            // XWin
+            // 
+            this.XWin.Location = new System.Drawing.Point(340, 48);
+            this.XWin.Name = "XWin";
+            this.XWin.Size = new System.Drawing.Size(100, 23);
+            this.XWin.TabIndex = 12;
+            this.XWin.Text = "label1";
+            // 
+            // OWin
+            // 
+            this.OWin.Location = new System.Drawing.Point(340, 139);
+            this.OWin.Name = "OWin";
+            this.OWin.Size = new System.Drawing.Size(100, 23);
+            this.OWin.TabIndex = 13;
+            this.OWin.Text = "label2";
+            // 
+            // Draws
+            // 
+            this.Draws.Location = new System.Drawing.Point(340, 230);
+            this.Draws.Name = "Draws";
+            this.Draws.Size = new System.Drawing.Size(100, 23);
+            this.Draws.TabIndex = 14;
+            this.Draws.Text = "label3";
+            // 
+            // TicTac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 352);
+            this.Controls.Add(this.Draws);
+            this.Controls.Add(this.OWin);
+            this.Controls.Add(this.XWin);
+            this.Controls.Add(this.EBotton);
+            this.Controls.Add(this.RButton);
+            this.Controls.Add(this.NGButton);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A00);
+            this.Name = "TicTac";
+            this.Text = "TicTac";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button NGButton;
+        private System.Windows.Forms.Button RButton;
+        private System.Windows.Forms.Button EBotton;
+        private System.Windows.Forms.Label XWin;
+        private System.Windows.Forms.Label OWin;
+        private System.Windows.Forms.Label Draws;
+    }
+}
+
